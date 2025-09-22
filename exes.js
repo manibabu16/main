977fe0cbcd9fc2797a5370fcafdffbc897a3ebd0
manibabu <manibabu16@gmail.com>
2025-09-22 07:14:59 +0530

    Add files via upload

---
+++ exes.js
@@ -0,0 +1,13 @@
+var season="winter"
+
+if(season=="summer"){
+    console.log("Have fun in the sun")
+}
+
+if(season=="spring"){
+    console.log("enjoy the day")
+}
+
+if(season=="winter"){
+    console.log("your winter season")
+}
