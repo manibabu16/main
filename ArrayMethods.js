0ba0500e291d3128c1c8e0353390aff32795a2af
manibabu <manibabu16@gmail.com>
2025-09-23 08:34:54 +0530

    Add files via upload

---
+++ ArrayMethods.js
@@ -0,0 +1,47 @@
+// javascript Array methods
+
+
+        // Push()
+// let array = ['a','b','c']
+// array.push('d')
+// console.log(array)
+
+        // Pop()
+// console.log(array.pop())
+// console.log(array)
+
+        // Shift()
+// console.log(array.shift())
+// console.log(array)
+
+        // unShift()
+// console.log(array.unshift('a'))
+// console.log(array)
+
+        // Splice()
+// let array = ['a','b','c','d']
+// array.splice(1,1, 'x')
+// console.log(array)
+
+        // reverse()
+// array.reverse()
+// console.log(array)
+
+        // forEach()
+// const fruits = ["apple","banana", "orange"]
+// fruits.forEach((fruit) => {console.log(fruit)})
+ 
+//         // Map()
+// let numbers = [1,2,3,4,5];
+// let squares = numbers.map((x)=>x*x);
+// console.log(squares);
+
+
+        //Array Methods/property
+
+// var country=['india', 'china']
+// console.log(country.toString())
+
+
+
+  
