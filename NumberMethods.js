62813c83c564c343b5cc087dd4a7780d38048816
manibabu <manibabu16@gmail.com>
2025-09-23 22:13:29 +0530

    Add files via upload

---
+++ NumberMethods.js
@@ -0,0 +1,32 @@
+// Number Methods in Js
+
+    // tofixed()
+// let num = 8.24515
+// console.log(num.toFixed(2))
+
+    // toString()
+// let num = 123;
+// console.log(num.toString());
+
+    // parseInt()
+// let str = "123";
+// console.log(parseInt(str))
+
+    // parseFloat()
+// let str = "152.25"
+// console.log(parseFloat(str))
+
+    // isNaN()
+// let num = "Hello"
+// console.log(isNaN(num))
+
+    // Math.random()
+// console.log(Math.random())
+
+    //Math.round()
+// let num = 9.99;
+// console.log(Math.round(num))
+
+    // math.max()  and math.min()
+// console.log(Math.max(1,2,3))
+// console.log(Math.min(6,5,4))
---
+++ StringMethods.js
@@ -0,0 +1,44 @@
+// String Methods in Js
+
+    // CharAt()
+// let text = "Web Design";
+// console.log(text.charAt(4))
+
+    // toUpperCase() / toLowerCase()
+// let text= "Hello world"
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase())
+
+    // indexOf()
+// let text = "Hello world"
+// console.log(text.indexOf("l"))
+
+    //substring()
+// let text = "Hello world"
+// console.log(text.substring(0,5))
+
+    // trim()
+// let text = "  Hello world  "
+// console.log(text.trim())
+
+    // split()
+// let text = "computer, pendrive, printer"
+// console.log(text.split(","))
+
+    // replace()
+// let text = "Hello World"
+// let Newtext = text.replace("World","Dear")
+// console.log(Newtext)
+
+    // includes()
+// let text = "Hello World"
+// console.log(text.includes("World"))
+
+    // concat()
+// let text1 = "Hello"
+// let text2 = "World"
+// console.log(text1.concat(" ", text2))
+
+    // repeat()
+// let text = "Hello "
+// console.log(text.repeat(2))
